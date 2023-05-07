f251f1591db4ede7e25a0834730c4465056cc483
Natalia Staśkiewicz <nataliastas1999@wp.pl>
2023-05-07 13:37:57 +0200

    The problem of eight queens

---
+++ aisd2/the problem of eights queens.py
@@ -0,0 +1,41 @@
+global n
+n = int(input('Podaj wielkość planszy\n n = '))
+
+def printSolution(board):
+    for i in range(n):
+        for j in range(n):
+            print(board[i][j], end=' ')
+        print()
+
+def isSafe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+def solveNQUtil(board, col):
+    if col >= n:
+        return True
+    for i in range(n):
+        if isSafe(board, i, col):
+            board[i][col] = 1
+            if solveNQUtil(board, col + 1) == True:
+                return True
+            board[i][col] = 0
+    return False
+
+def solveNQ():
+    board = [[0 for col in range(n)] for row in range(n)]
+    if solveNQUtil(board, 0) == False:
+        print("Rozwiązanie nie istnieje")
+        return False
+    printSolution(board)
+    return True
+
+solveNQ()
